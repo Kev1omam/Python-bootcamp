3c72bb76dfed649eaad156994cf64b1b4ea78fc9
kev1omam <omambaby217@gmail.com>
2022-04-20 19:24:24 +0100

    js

---
+++ week3/day 5/daily challenge.js
@@ -0,0 +1,8 @@
+let sentence = "'This dinner is not that bad ! You cook well',"
+let startIndex = sentence.search("not");
+let EndIndex = sentence.search("bad");
+let EndIndex = sentence.search("bad") + 3 ;
+let array = sentence.split("");
+startIndex = array.indexOf('bad');
+EndIndexInd = array.indexOf("bad");
+array.splice("startInd , (E)")
