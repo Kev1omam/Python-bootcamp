ba487f78abd0fe19e400c429e08e890c13241317
kev1omam <omambaby217@gmail.com>
2022-04-20 20:04:50 +0100

    exo js

--- week3/day 4/exo.js
+++ week3/day 4/exo.js
@@ -1,1 +1,21 @@
-( let i = 0 ; i < 15 ; i ++;) if ( i%2 === 0 ) { console.log(i + le " nombre est paire")}else if (i%2 !==0){ console.log( i +'le nombre est impaire')};
+( let i = 0 ; i <= 15 ; i ++;) {if ( i%2 === 1 ) { console.log(`${i} + le " nombre est paire")} else { console.log(${i}))`} 
+let names= ["john", "sarah", 23, "Rudolf",34]
+  for (let  index of names) {
+
+    if (typeof(index) !== 'string' ){
+      continue;
+    }else if (index[0]  !== index[0].toLocaleUpperCase()){
+      index.replace(index[0], index[0].toLocaleUpperCase());
+      console.log(index);
+
+    }
+  }
+  for ( let index of names){
+
+
+    if(typeof(index) !=="string"){
+      break;
+    }else {
+      console,log(index);
+    }
+  }
