613e6b8050b44d6f82512364dc8f32de578f82ba
kev1omam <omambaby217@gmail.com>
2022-05-04 17:24:26 +0100

    python

---
+++ week6/daylichallenge/dayli.py
@@ -0,0 +1,7 @@
+a = input("entrer une phrase: ")
+if len(a) < 10 : 
+  print("string not long enough")
+else:
+  print("string too long")  
+  print(a[0:-1])
+
---
+++ week6/daylichallenge/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+a = input("entrer une phrase: ")
+b = len(a)
+if b < 10 : 
+  print("string not long enough")
+else:
+  print("string too long") 
